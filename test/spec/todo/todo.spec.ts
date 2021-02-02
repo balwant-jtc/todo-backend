217d3d80730de2cbf2e41e3dad8bf176665bec61
balwant-jtc <balwant.singh@jalantechnologies.com>
2021-02-02 15:02:29 +0530

    remove commented code

--- test/spec/todo/todo.spec.ts
+++ test/spec/todo/todo.spec.ts
@@ -1,16 +1,16 @@
 // tslint:disable-next-line: no-var-requires
-require('module-alias/register');
+require("module-alias/register");
 
-import chai from 'chai';
+import chai from "chai";
 // tslint:disable-next-line: import-name
-import spies from 'chai-spies';
+import spies from "chai-spies";
 chai.use(spies);
-import chaiHttp from 'chai-http';
-import { Application } from 'express';
-import { respositoryContext, testAppContext } from '../../mocks/app-context';
+import chaiHttp from "chai-http";
+import { Application } from "express";
+import { respositoryContext, testAppContext } from "../../mocks/app-context";
 
-import { App } from '@server';
-import { Todo } from '@models';
+import { App } from "@server";
+import { Todo } from "@models";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -25,61 +25,33 @@ before(async () => {
   expressApp = app.expressApp;
 });
 
-describe('POST /todos', () => {
-  it('should create a new todo', async () => {
-    const res = await chai
-      .request(expressApp)
-      .post('/todos')
-      .send({
-        title: 'New Todo'
-      });
+describe("POST /todos", () => {
+  it("should create a new todo", async () => {
+    const res = await chai.request(expressApp).post("/todos").send({
+      title: "New Todo",
+    });
 
     expect(res).to.have.status(201);
-    expect(res.body).to.have.property('id');
-    expect(res.body).to.have.property('title');
+    expect(res.body).to.have.property("id");
+    expect(res.body).to.have.property("title");
   });
 
-  it('should return a validation error if empty title is specified', async () => {
-    const res = await chai
-      .request(expressApp)
-      .post('/todos')
-      .send({
-        title: ''
-      });
+  it("should return a validation error if empty title is specified", async () => {
+    const res = await chai.request(expressApp).post("/todos").send({
+      title: "",
+    });
 
     expect(res).to.have.status(400);
   });
 
-  it('should return a validation error if title is not a string', async () => {
+  it("should return a validation error if title is not a string", async () => {
     const res = await chai
       .request(expressApp)
-      .post('/todos')
+      .post("/todos")
       .send({
-        title: {"key":"value"}
+        title: { key: "value" },
       });
 
     expect(res).to.have.status(400);
   });
-
-  /* it('should return an error for duplicate email', async () => {
-    await testAppContext.accountRepository.save(new Account({
-      email: 'Jack.Ryan.2@jalantechnologies.com',
-      firstName: 'Jack',
-      lastName: 'Ryan',
-      password: await AuthHelper.encryptPassword('password'),
-    }));
-
-    const res = await chai
-      .request(expressApp)
-      .post('/account')
-      .send({
-        email: 'Jack.Ryan.2@jalantechnologies.com',
-        firstName: 'Jack',
-        lastName: 'Ryan',
-        password: 'password',
-      });
-    expect(res).to.have.status(400);
-  }); */
 });
-
-
