1439fcc9d2fa46b18ced4b5703d62e2b1b24d1cd
balwant-jtc <78337443+balwant-jtc@users.noreply.github.com>
2021-02-02 18:01:42 +0530

    Endpoint to create a new TODO item added (#1)
    * Endpoint to create a new TODO item added
    
    * remove commented code
    
    * PR review changes added

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,9 @@
-import { Account } from './account';
+import { Todo } from './todo';
 import { BaseModel } from './base-model';
 import { ModelFactory } from './model-factory';
 
 export {
-  Account,
+  Todo,
   BaseModel,
   ModelFactory,
 };
